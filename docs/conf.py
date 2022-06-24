cf34d4c603a846fcfbdca5bcf10a8ba3a8a02aba
Raphaël Rey <raphael.rey@slsp.ch>
2022-06-24 18:35:29 +0200

    update doc

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Raphaël Rey'
 author = 'Raphaël Rey'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0'
+release = '0.3'
 
 
 # -- General configuration ---------------------------------------------------
