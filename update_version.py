99d7cdf08140e225e66a139f3b64c80c84519dd7
Raphaël Rey <raphael.rey@slsp.ch>
2022-08-22 15:45:29 +0200

    Create version 0.6.0

--- update_version.py
+++ update_version.py
@@ -1,5 +1,7 @@
 import almapiwrapper
 import re
+import subprocess
+import os
 
 version = almapiwrapper.__version__
 
@@ -29,3 +31,12 @@ content = re.sub(r'\* Version: \d+\.\d+\.\d+', f'* Version: {version}', content)
 
 with open('docs/index.rst', 'w') as f:
     f.write(content)
+
+os.system(f'{os.getcwd()}/docs/make.bat html')
+os.system(f'{os.getcwd()}/docs/make.bat html')
+
+os.system('git add .')
+os.system(f'git commit -m "Create version {version}"')
+# os.system(f'git push')
+#
+# os.system('twine upload dist/*')
