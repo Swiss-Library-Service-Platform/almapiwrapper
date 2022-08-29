6cd33e821caed96b11ad6f40f4aa3b3d0521cc02
Raphaël Rey <raphael.rey@slsp.ch>
2022-08-29 12:07:12 +0200

    Update update_version.py doc

--- update_version.py
+++ update_version.py
@@ -32,11 +32,22 @@ content = re.sub(r'\* Version: \d+\.\d+\.\d+', f'* Version: {version}', content)
 with open('docs/index.rst', 'w') as f:
     f.write(content)
 
+# Build the new doc
 os.system(f'{os.getcwd()}/docs/make.bat html')
 os.system(f'{os.getcwd()}/docs/make.bat html')
+
+# Delete all files of dist folder
+files = os.listdir('dist')
+for f in files:
+    os.remove(f'dist/{f}')
+
+# Build the new package
 os.system('python -m build')
+
+# Commit the new version
 os.system('git add .')
 os.system(f'git commit -m "Create version {version}"')
 os.system(f'git push')
-#
+
+# Upload the package on pipy
 os.system('twine upload dist/*')
