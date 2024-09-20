4df036856c932e385e8856cca4058f9f8bf6bd57
Raphaël Rey <raphael.rey@slsp.ch>
2024-09-20 22:32:53 +0200

    Create version 1.1.8

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Raphaël Rey'
 author = 'Raphaël Rey'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.7'
+release = '1.1.8'
 
 
 # -- General configuration ---------------------------------------------------
