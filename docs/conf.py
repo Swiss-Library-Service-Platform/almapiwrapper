47c36a3ca501679b97b4e508f4bb268ebe17153a
Raphaël Rey <raphael.rey@slsp.ch>
2026-05-04 15:20:40 +0200

    Create version 1.7.5: Clean documentation

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Raphaël Rey'
 author = 'Raphaël Rey'
 
 # The full version, including alpha/beta/rc tags
-release = '1.7.4'
+release = '1.7.5'
 
 
 # -- General configuration ---------------------------------------------------
